8fb48bd13464131199f9862a997cb07520fa4435
Andre Austin <austinaj2@vcu.edu>
2022-04-28 16:26:22 -0400

    lets get it started

--- Peer Trivia/ViewController.swift
+++ Peer Trivia/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, MCBrowserViewControllerDelegate, MCSessionDelegate, UINavigationControllerDelegate  {
+    
+    var session = MCSession()
+    var peerID = MCPeerID()
+    var browser = MCBrowserViewController()
+    var assistant = MCAdvertiserAssistant()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.peerID = MCPeerID(displayName: UIDevice.current.name)
+        self.session = MCSession(peer: peerID)
+        self.browser = MCBrowserViewController(serviceType: "chat", session: session)
+        self.assistant = MCAdvertiserAssistant(serviceType: "chat", discoveryInfo: nil, session: session)
+        
+        assistant.start()
+        session.delegate = self
+        browser.delegate = self
     }
-
-
+    /* required MCBrowser functions */
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        <#code#>
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        <#code#>
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        <#code#>
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        <#code#>
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        <#code#>
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        <#code#>
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        <#code#>
+    }
 }
 
